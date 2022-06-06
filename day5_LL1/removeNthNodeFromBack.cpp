ee5b1e7f1285afc39217d042eb7751cb820f6cbb
Shubhams-2002 <sssalunkhe_b20@ce.vjti.ac.in>
2022-06-06 12:02:56 +0530

    day5_q4

---
+++ day5_LL1/removeNthNodeFromBack.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+LinkedListNode<int>* removeKthNode(LinkedListNode<int> *head, int K)
+{
+    if(head==NULL || K==0) return head;
+    
+    LinkedListNode<int>* back=head;
+    LinkedListNode<int>* front=head;
+    LinkedListNode<int>* to_delete;
+    while(K && front) {
+        front = front->next;
+        K--;
+    }
+    if(front==NULL) {
+        to_delete = head;
+        head=head->next;
+        delete to_delete;
+        return head;
+    }
+    while(front->next) {
+        back=back->next;
+        front=front->next;
+    }
+    to_delete = back->next;
+    back->next = back->next->next;
+    delete to_delete;
+    
+    return head;
+}
