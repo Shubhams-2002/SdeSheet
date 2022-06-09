18bfcb0a7c53d04f7bb6ac918e8a7549824d83c6
Shubhams-2002 <sssalunkhe_b20@ce.vjti.ac.in>
2022-06-09 09:08:06 +0530

    day7_q5

--- day6_LL2/pallindromeLL.cpp
+++ day6_LL2/pallindromeLL.cpp
@@ -17,8 +17,48 @@
     };
 
 *****************************************************************/
-
+LinkedListNode<int> *reverse(LinkedListNode<int> *head) 
+{
+    LinkedListNode<int>* prev = NULL;
+    LinkedListNode<int>* curr = head;
+    LinkedListNode<int>* nxt;
+    
+    while(curr) {
+        nxt = curr->next;
+        
+        curr->next = prev;
+        
+        prev = curr;
+        curr = nxt;
+    }
+    
+    return prev;
+}
 bool isPalindrome(LinkedListNode<int> *head) {
+    if(head==NULL || head->next==NULL) {
+        return true;
+    }
+    LinkedListNode<int>* slow = head;
+    LinkedListNode<int>* fast = head;
+    
+    while(fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    slow->next = reverse(slow->next);
+    slow = slow->next;
+    LinkedListNode<int>* temp = head;
+    while(slow) {
+        if(slow->data != temp->data) {
+            return false;
+        }
+        slow = slow->next;
+        temp = temp->next;
+    }
+    return true;
+}
+bool isPalindrome1(LinkedListNode<int> *head) {
     if(head==NULL) return true;
     LinkedListNode<int>* temp = head;
     LinkedListNode<int>* prev = NULL;
