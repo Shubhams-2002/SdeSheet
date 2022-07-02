e1e9c0f8c14f6ed9641649728b22c90678a97e6f
Shubhams-2002 <sssalunkhe_b20@ce.vjti.ac.in>
2022-07-02 12:46:02 +0530

    day18_q8_boundary_traversal

---
+++ Trees/L20_boundary_traversal.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+// Functions to traverse on each part.
+void leftBoundary(TreeNode<int>* root, vector<int>& ans);
+void rightBoundary(TreeNode<int>* root, vector<int>& ans);
+void leafNodes(TreeNode<int>* root, vector<int>& ans);
+
+vector<int> traverseBoundary(TreeNode<int>* root){
+    vector<int> ans;
+
+    if(root == NULL){
+        return ans;
+    }
+     
+    ans.push_back(root -> data);
+    
+    // Traverse left boundary.
+    leftBoundary(root -> left, ans);
+    
+    // Traverse for leaf nodes.
+    leafNodes(root -> left, ans);
+    leafNodes(root -> right, ans);
+    
+    // Traverse right boundary.
+    rightBoundary(root -> right, ans);
+    
+    return ans;
+}
+
+void leftBoundary(TreeNode<int>* root, vector<int>& ans){
+    if(root == NULL || (root -> left == NULL && root -> right == NULL)){
+        return;
+    }
+    
+    ans.push_back(root -> data);
+    
+    if(root -> left != NULL){
+        leftBoundary(root -> left, ans);
+    }
+
+    else{
+        leftBoundary(root -> right, ans);
+    }
+}
+
+void rightBoundary(TreeNode<int>* root, vector<int>& ans){
+    if(root == NULL || (root -> left == NULL && root -> right == NULL)){
+        return;
+    }
+    
+    if(root -> right != NULL){
+        rightBoundary(root -> right, ans);
+    }
+
+    else{
+        rightBoundary(root -> left, ans);
+    }
+    
+    ans.push_back(root -> data);
+}
+
+void leafNodes(TreeNode<int>* root, vector<int>& ans){
+    if(root == NULL){
+        return;
+    }
+    
+    if(root -> left == NULL && root -> right == NULL){
+        ans.push_back(root -> data);
+        return;
+    }
+    
+    leafNodes(root -> left, ans);
+    leafNodes(root -> right, ans);
+}
