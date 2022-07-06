29465459e11800a3e64566bcc2fb2a9088e1c425
Shubhams-2002 <sssalunkhe_b20@ce.vjti.ac.in>
2022-07-06 14:42:27 +0530

    tree-series-L32
    count-all-nodes-in-CBT

--- Trees/L32-count-all-nodes-in-CBT.cpp
+++ Trees/L32-count-all-nodes-in-CBT.cpp
@@ -1,1 +1,34 @@
 #include<bits/stdc++.h>
+class Solution {
+    int leftHeight(TreeNode* root) {
+        TreeNode* node = root->left;
+        int h=0;
+        while(node) {
+            h++;
+            node = node->left;
+        }
+        return h;
+    }
+    
+    int rightHeight(TreeNode* root) {
+        TreeNode* node = root->right;
+        int h=0;
+        while(node) {
+            h++;
+            node = node->right;
+        }
+        return h;
+    }
+    
+public:
+    int countNodes(TreeNode* root) {
+        if(!root) return 0;
+        
+        int lh = leftHeight(root);
+        int rh = rightHeight(root);
+        
+        if(lh==rh) return pow(2,lh+1)-1;
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
